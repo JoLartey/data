d5bfcc023c671a42e1185efbb5e5186b50061d4a
Joshua Ansah <joeansah2012@gmail.com>
2020-10-09 13:53:43 +0000

    Slightly enhanced the greeting code

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('hello world')
+# This code welcomes the fellow to cohort 2 (C2) of the Notitia-AI mentorship programme. The programme first says
+# hello, then goes on to ask for the name before welcoming the fellow
+
+
+print('Hello Notitia-AI C2 Fellow')
+
+
+# The greet function requests the name of the fellow and then welcomes them to the fellowship.
+def greet():
+    name = input("Please enter your name\n")
+    print("Welcome to Notitia-AI" + "" + name)
+
+
+greet()
